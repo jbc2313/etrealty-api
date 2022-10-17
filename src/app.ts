e767ab3a9d3affa424c7bf38842920b3422e14b7
jb corbin <101614021+jbc2313@users.noreply.github.com>
2022-10-16 20:12:22 -0500

    working on fetching props

--- src/app.ts
+++ src/app.ts
@@ -11,8 +11,6 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 
-
-
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
     res.json({ message: "Express with TypeScript!"})
 });
--- src/fetchPropertys.ts
+++ src/fetchPropertys.ts
@@ -12,21 +12,82 @@ const options = {
 
 export const getPropertys = async () => { 
     const { data } = await axios.request(options)
-    console.log(data.data)
+//    console.log(data.data.home_search)
+    const totalApiProperty = data.data.home_search.total // total number of propertys in api
+    const apiProps = data.data.home_search.results // array of propertys
+    console.log("props =>", apiProps)
+    console.log("Total Props", totalApiProperty)
+
 }
 
 
-/* Current results format... pickup here.. 
+
+
+
+
+
+/* Current shape of data returned from data.data.home_search
+
 {
-  home_search: { total: 66, count: 2, results: [ [Object], [Object] ] },
-  geo: {
-    parents: [ [Object], [Object], [Object] ],
-    recommended_zips: { geos: [Array] },
-    recommended_cities: { geos: [Array] },
-    recommended_counties: { geos: [Array] },
-    geo_statistics: { housing_market: [Object] },
-    recommended_neighborhoods: { geos: [Array] }
-  }
+  total: 66,
+  count: 2,
+  results: [
+    {
+      primary_photo: [Object],
+      last_update_date: '2022-10-14T09:35:20Z',
+      source: [Object],
+      tags: [Array],
+      permalink: '441-E-8th-St_Adrian_MO_64720_M84514-33077',
+      status: 'for_sale',
+      list_date: '2022-10-14T15:36:54Z',
+      open_houses: null,
+      tax_record: [Object],
+      branding: [Array],
+      photos: [Array],
+      coming_soon_date: null,
+      list_price: 149900,
+      matterport: false,
+      property_id: '8451433077',
+      flags: [Object],
+      lead_attributes: [Object],
+      community: null,
+      products: [Object],
+      virtual_tours: null,
+      description: [Object],
+      listing_id: '2948892151',
+      price_reduced_amount: null,
+      location: [Object],
+      other_listings: [Object]
+    },
+    {
+      primary_photo: [Object],
+      last_update_date: '2022-10-02T20:42:13Z',
+      source: [Object],
+      tags: [Array],
+      permalink: '13900-355th-St_Adrian_MO_64720_M88882-09488',
+      status: 'for_sale',
+      list_date: '2022-10-03T01:39:35Z',
+      open_houses: null,
+      tax_record: [Object],
+      branding: [Array],
+      photos: [Array],
+      coming_soon_date: null,
+      list_price: 885000,
+      matterport: false,
+      property_id: '8888209488',
+      flags: [Object],
+      lead_attributes: [Object],
+      community: null,
+      products: [Object],
+      virtual_tours: null,
+      description: [Object],
+      listing_id: '2948460281',
+      price_reduced_amount: null,
+      location: [Object],
+      other_listings: [Object]
+    }
+  ]
 }
+
 */
 
