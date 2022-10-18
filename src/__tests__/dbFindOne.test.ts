72857097ac5814fce7fd8f4ac4b3284b67f4a8cb
jb corbin <101614021+jbc2313@users.noreply.github.com>
2022-10-17 23:08:05 -0500

    added test for db query

---
+++ src/__tests__/dbFindOne.test.ts
@@ -0,0 +1,13 @@
+import { describe, expect, test, beforeAll } from '@jest/globals'
+import prisma from "../dbUtils/prismaClient"
+
+beforeAll(() => {
+    //setup enviroment for test here
+})
+
+describe('query a single Property model from db', () => {
+    test(('prisma query should return 1 property'), () => {
+        expect(prisma.property.findFirst({where: { PropertyId: { equals: "8888209488"}}})).resolves.toHaveProperty("PropertyId", "8888209488")
+    })
+})
+
