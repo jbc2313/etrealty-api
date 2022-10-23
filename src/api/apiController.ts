650b93d224537cda96d797ce0f7e9fa4fb0e8759
jb corbin <101614021+jbc2313@users.noreply.github.com>
2022-10-23 09:13:54 -0500

    added prisma to controller

--- src/api/apiController.ts
+++ src/api/apiController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express'
-
+import prisma from '../dbUtils/prismaClient'
 
 export const getAllProperty = (req: Request, res: Response) => {
     console.log('getAllProperty Route Pinged.')
