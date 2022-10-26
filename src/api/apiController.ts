6043bfde9186992b970a603c0ff71c12d4631b1b
jb corbin <101614021+jbc2313@users.noreply.github.com>
2022-10-25 22:42:30 -0500

    updated find single property api route

--- src/api/apiController.ts
+++ src/api/apiController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 import prisma from '../dbUtils/prismaClient'
+import { findProperty } from '../dbUtils/findSingleModel' 
 
 export const getAllProperty = (req: Request, res: Response) => {
     console.log('getAllProperty Route Pinged.')
@@ -8,5 +9,11 @@ export const getAllProperty = (req: Request, res: Response) => {
 
 export const getSingleProperty = (req: Request, res: Response) => {
     console.log('getSingleProperty Route Pinged.')
+    console.log(req.body)
+    //const query = req.body.propId
+    //findProperty(query)
+    //.then(res => {
+
+    //})
     res.json({msg: "SINGLE HOUSE ROUTE"})
 }
