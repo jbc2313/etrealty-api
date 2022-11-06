05bfcf6c4e1e113c65f8cc25990375e7ae4d2126
jb corbin <101614021+jbc2313@users.noreply.github.com>
2022-11-05 19:34:58 -0500

    dbmodelcheck is returning lastupdate

--- src/dbUtils/checkDbModelsSold.ts
+++ src/dbUtils/checkDbModelsSold.ts
@@ -9,8 +9,24 @@ export async function checkSold() {
     const dbModels = await prisma.property.findMany();
     const apiModels = await getPropertys()
 
-    console.log("DB MODELS => ", dbModels);
-    console.log("API MODELS => ", apiModels);
+    //console.log("DB MODELS => ", dbModels);
+    //console.log("API MODELS => ", apiModels);
+    
+    const totaldb = dbModels.length;
+    const totalapi = apiModels.apiProps.length;
+
+    console.log(`total from db = ${totaldb}, total from api = ${totalapi}`);
+    
+    const dbLastUpdate = dbModels.map((mod: any)=> {
+        return mod.ApiUpdateAt;
+    })
+
+    const apiLastUpdate = apiModels.apiProps.map((mod: any)=> {
+        return mod.last_update_date;
+    })
+
+    console.log(`db list = ${dbLastUpdate}, api list = ${apiLastUpdate}`)
+    
      
 }
 
