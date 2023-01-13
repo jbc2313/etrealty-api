a3f63f5373d5d09b32e6f7434948c382002b9cca
jb corbin <101614021+jbc2313@users.noreply.github.com>
2023-01-13 12:30:09 -0600

    working on formating time for updated time

--- src/dbUtils/unlistDbModel.ts
+++ src/dbUtils/unlistDbModel.ts
@@ -1,6 +1,37 @@
+import prisma from "./prismaClient";
+
 export async function unlistModel(list: any) {
     console.log("**DB MODEL UNLISTED**");
     console.log("unlist list");
+    //console.log(list);
+    //time format for last updated
+    //   2022-12-13T15:01:02Z
+    
+    // NEED TO GET TIME TO THIS FORMAT ABOVE
+    const time: any = Date.now();
+    console.log("TIME IS:");
+    console.log(time);
+
+    // list.forEach((el: string)=> {
+    //     prisma.property.update({
+    //         where: { PropertyId: el },
+    //         data: { 
+    //             Status: "Unlisted", 
+    //             ApiUpdateAt: " "
+    //         } 
+    //     })        
+    // })
+
+
+
+    // just view the propertys in the db
+    // list.forEach((el: string)=> {
+    //     prisma.property.findMany({
+    //         where: { PropertyId: el}
+    //     }).then(response => {
+    //         console.log(response)
+    //     })
+    // })
 
 
 };
--- src/dbUtils/updateDbModel.ts
+++ src/dbUtils/updateDbModel.ts
@@ -6,5 +6,5 @@ export async function updateInfo(dbArr: any, apiArr: any, dbList: any) {
     console.log('-------------');
     console.log("API ARRAY BELOW");
     console.log(apiArr);
-    console.log("**UPDATED DB INFO WITH NEW INFO FROM API");
+    console.log("##**##UPDATED DB INFO WITH NEW INFO FROM API");
 }
