7a1e180be7c4cc67e4a78e1f1bd8026bea1264b8
jb corbin <101614021+jbc2313@users.noreply.github.com>
2023-01-14 08:21:13 -0600

    updated unlist function

--- src/dbUtils/updateDbModel.ts
+++ src/dbUtils/updateDbModel.ts
@@ -2,9 +2,9 @@ export async function updateInfo(dbArr: any, apiArr: any, dbList: any) {
     console.log("LIST OF IDS NEED UPDATED");
     console.log(dbList);
     console.log("DB ARRAY BELOW"); 
-    console.log(dbArr);
+    //console.log(dbArr);
     console.log('-------------');
     console.log("API ARRAY BELOW");
-    console.log(apiArr);
+    //console.log(apiArr);
     console.log("##**##UPDATED DB INFO WITH NEW INFO FROM API");
 }
