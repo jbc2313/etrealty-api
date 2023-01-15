4c39bae5c9e002c360f22e3b26c5da43f5514b25
jb corbin <101614021+jbc2313@users.noreply.github.com>
2023-01-15 15:00:17 -0600

    update db is now working, need to create tests/debug

--- src/dbUtils/updateDbModel.ts
+++ src/dbUtils/updateDbModel.ts
@@ -1,36 +1,40 @@
 import prisma from "./prismaClient";
-
+import { checkProp } from "./checkApiProperty";
 
 export async function updateInfo(dbArr: any, apiArr: any, dbList: any) {
     console.log("LIST OF IDS NEED UPDATED");
     console.log(dbList);
     //console.log("DB ARRAY BELOW"); 
     //console.log(dbArr);
-    console.log('-------------');
+   // console.log('-------------');
     //console.log("API ARRAY BELOW");
-    console.log(apiArr);
+    //console.log(apiArr);
     
     for (const el of dbList) {
-        console.log("From for loop:"); 
-        console.log(el); 
+        //console.log("From for loop:"); 
+        //console.log(el); 
         
         const newInfo = apiArr.apiProps.filter((record: any)=> record.property_id === el )
-        console.log("## NEW API INFO ##");
-        console.log(newInfo);
+        //console.log("## NEW API INFO ##");
+        //console.log(newInfo);
+        const preparedInfo = await checkProp(newInfo[0])
+        //console.log(preparedInfo);
 
         // Code to update db property
-        // prisma.property.update({
-        //     where: { PropertyId: el },
-        //     data: {
-        //         ApiUpdateAt:
-        //     }
-        // })
-
-        console.log("##**##UPDATED DB INFO WITH NEW INFO FROM API");
-
+        prisma.property.update({
+            where: { PropertyId: el },
+            data: {
+                ApiUpdateAt: preparedInfo.ApiUpdateAt,
+                ListPrice: preparedInfo.ListPrice,
+                Status: preparedInfo.Status,
+                ListDate: preparedInfo.ListDate,
+                Photos: preparedInfo.Photos,
+            }
+        }).then(returned => console.log("**db model updated**"));
 
     }
 
 
 
+    console.log("##**##UPDATED DB INFO WITH NEW INFO FROM API");
 }
