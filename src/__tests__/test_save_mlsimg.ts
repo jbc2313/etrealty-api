840e702b2f7b12a87c6490dbfb4c4fc30d791ab1
jb corbin <101614021+jbc2313@users.noreply.github.com>
2023-08-29 12:08:28 -0500

    img download deals with broken mlsimgurl

--- src/__tests__/test_save_mlsimg.ts
+++ src/__tests__/test_save_mlsimg.ts
@@ -1,9 +1,12 @@
 import { convertImage } from "../utils/convertImage";
 
 
+const tmpurl = "https://www.publicdomainpictures.net/pictures/500000/velka/vintage-kunst-musik-katzen.jpg"
+const mlsid = "mlsidgoeshere";
+const arrayidx = 1;
 
 export async function testConvert() {
-    const res = await convertImage();
+    const res = await convertImage(tmpurl, mlsid, arrayidx);
     return res;
 
 };
--- src/utils/convertImage.ts
+++ src/utils/convertImage.ts
@@ -1,5 +1,5 @@
 import { createObject } from './putS3Object';
-import { writeFile } from 'node:fs/promises';
+import { writeFile, mkdir } from 'node:fs/promises';
 //const fsPromises = fs.promises;
 //
 import axios from 'axios';
@@ -12,42 +12,70 @@ import path from 'path';
 // put the s3 link in the Media key of the property.
 // the MEdia key is json so we can save a string array of multiple image urls
 
+const folderpath = "../../imgfolder/";
+
 //test image link
 // https://www.google.com/imgres?imgurl=https%3a%2f%2fupload.wikimedia.org%2fwikipedia%2fcommons%2fthumb%2f4%2f4f%2fneovim-logo.svg%2f2560px-neovim-logo.svg.png&tbnid=6umabjwefiu7om&vet=12ahukewjxyf-41_-aaxx-o94ahc3razsqmyg_eguiarcfag..i&imgrefurl=https%3a%2f%2fcommons.wikimedia.org%2fwiki%2ffile%3aneovim-logo.svg&docid=mons9hlxixiolm&w=2560&h=738&q=neovim&hl=en&ved=2ahukewjxyf-41_-aaxx-o94ahc3razsqmyg_eguiarcfag
 
+// working url
 const tmpurl = "https://www.publicdomainpictures.net/pictures/500000/velka/vintage-kunst-musik-katzen.jpg"
 
+// borked url for testing broken link
+//const tmpurl = "https://www.publicdopictures.net/pictures/500000/velka/vintage-kunst-musik-katzen.jpg"
 
-export async function convertImage() {
-    const mlsimg = await tmpImg(tmpurl, "11111111_", "../../testfiles/");
 
-    return mlsimg;
+
+// have to make this function take in mlsurl, mlsid, idx of img array(for appending filename), 
+export async function convertImage(mlsurl: string, mlsid: string, arrayidx: number) {
+
+
+    let imgres=makeFolder().then((res:any)=>{
+        const val=tmpImg(mlsurl, mlsid, arrayidx.toString(), folderpath).then((res:any)=>{
+            return res;
+        })
+        return val;
+    })
+
+    return imgres;
+
 
 };
 
+async function makeFolder() {
+   const res = await mkdir(path.resolve(__dirname, folderpath), {recursive: true});
+   return res;
+};
 
-async function tmpImg(url: string, mlsid: string, file_path: string) {
-    console.log('axios about to run');
-    // have to use ts ignore due to github issue 
-    // https://github.com/DefinitelyTyped/DefinitelyTyped/issues/60924
-    const fileResponse = await axios({
-        url,
-        method: 'GET',
-        responseType: 'arraybuffer',
-    });
+async function tmpImg(url: string, mlsid: string, arrayidx:string, file_path: string) {
+    
+    
+        console.log('mlsimg download starting');
+        const fileResponse = await axios({
+            url,
+            method: 'GET',
+            responseType: 'arraybuffer',
+        }).catch(function (error) {
+            console.log(error.toJSON());
+            return error.toJSON();
+        });
 
-    const res = await writeFile(path.resolve(__dirname, file_path) + mlsid + "uniquename.jpg", fileResponse.data)
-   
-    if (res == undefined){
-        let msg = path.resolve(__dirname, file_path)+mlsid+"uniquename.jpg";
-        console.log(`${msg} was temporarily saved.`);
-        return msg;
-    };
+        if (fileResponse.name == "Error"){
+            return "MLS IMG URL IS BROKEN, MOVING ON";
+        }else{
+            const res = await writeFile(path.resolve(__dirname, file_path) + "/" + mlsid + "-" + arrayidx +".jpg", fileResponse.data)
+       
+            if (res == undefined){
+                let msg = path.resolve(__dirname, file_path)+ "/" + mlsid + "-" + arrayidx + ".jpg";
+                console.log(`${msg} was temporarily saved.`);
+                return msg;
+            };
 
-    return res;
-   
+            return res;
+       
+        }
 
 
+};
 
 // could be implemented with fetch if node and fetch fucking working properly with typscript
     /*
@@ -61,4 +89,3 @@ async function tmpImg(url: string, mlsid: string, file_path: string) {
 */
 
 
-};
